7abc6de0c10a1c2d1c52d99d1e4d0556ed78d51b
João Otávio <joaotavioos@outlook.com>
2022-08-07 22:06:21 -0300

    feat: add kafka customers producer ✨

--- microservice/app/models/customer.rb
+++ microservice/app/models/customer.rb
@@ -1,3 +1,15 @@
 class Customer < ApplicationRecord
     has_many :orders
+
+    after_create :set_queue
+    after_update :set_queue
+
+    private
+
+    def set_queue
+        Karafka.producer.produce_async(
+            topic: 'customers',
+            payload: self.to_json
+          )
+    end
 end
