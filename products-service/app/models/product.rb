c92f170a96a99d0d5cf4d98242cf11e704823d44
João Otávio <joaotavioos@outlook.com>
2022-08-07 22:07:00 -0300

    feat: add kafka products producer ✨

--- products-service/app/models/product.rb
+++ products-service/app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+    after_create :set_queue
+    after_update :set_queue
+
+    private
+
+    def set_queue
+        Karafka.producer.produce_async(
+            topic: 'products',
+            payload: self.to_json
+          )
+    end
 end
